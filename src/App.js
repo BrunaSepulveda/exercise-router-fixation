8e8c4be7e5e5b3222bf3548acbb0b68b88fb7f42
Bruna Sepulveda <sepulvedabruna@gmail.com>
2020-11-18 16:56:36 -0300

    primeira etapa

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import Home from './Home';
+import About from './About';
+import Users from './Users';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+      <Route exact path="/"><Home /></Route>
+      <Route path="/about"><About /></Route>
+      <Route path="/users"><Users /></Route>
+      </BrowserRouter>
     );
   }
 }
